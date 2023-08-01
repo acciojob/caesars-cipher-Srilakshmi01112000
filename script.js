2ef29f24fe2974537635e10314523d4844cf14bc
Sri Lakshmi Iyengar K R <110050350+Srilakshmi01112000@users.noreply.github.com>
2023-08-01 11:17:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,7 +34,30 @@ const lookup = {
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
+ for (let i = 0; i < encodedString.length; i++) {
+    const char = encodedString[i];
 
+    // Check if the character is an uppercase letter
+    if (/[A-Z]/.test(char)) {
+      // Get the character code and decode it
+      let charCode = encodedString.charCodeAt(i);
+      let decodedCharCode = charCode - 13;
+
+      // If the decoded character code is less than 'A', wrap around to the end of the alphabet
+      if (decodedCharCode < 65) {
+        decodedCharCode += 26;
+      }
+
+      // Convert the decoded character code back to a character and push it to the decodeArr
+      decodeArr.push(String.fromCharCode(decodedCharCode));
+    } else {
+      // For non-alphabetic characters, just push them to the decodeArr as they are
+      decodeArr.push(char);
+    }
+  }
+
+  // Join the characters in decodeArr to form the decoded string
+  const decodedString = decodeArr.join('');
   return; //return decodedArr
 }
 
